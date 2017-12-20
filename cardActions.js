ee19683f57c615e1c1899c97924c1e0112e1992f
IKStreamIvo <ivostreaming@gmail.com>
2017-12-20 17:10:35 +0100

    test

---
+++ cardActions.js
@@ -0,0 +1,21 @@
+var cardActions = {
+    //Card Actions
+	homecoming: function(cardData){
+        console.log('homecoming');
+	},
+	destroyPointCard: function(cardData){
+
+	},
+	protectPointCard: function(cardData){
+
+	},
+	skipTurn: function(cardData){
+
+	},
+	stealHandCard: function(cardData){
+
+	},
+	drawTwoCards: function(cardData){
+
+	},
+}
